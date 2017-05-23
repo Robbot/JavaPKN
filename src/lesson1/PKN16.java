d71dde4222a235cd4771dc5c3cadcc051abdb27d
Rob_R <roju@robertrojek.pl>
2017-05-23 17:51:52 +0100

    PKN16

---
+++ src/lesson1/PKN16.java
@@ -0,0 +1,18 @@
+package lesson1;
+//https://www.youtube.com/watch?v=o5dcpcuyuIA&list=PLS1QulWo1RIbfTjQvTdj8Y6yyq4R7g-Al&index=16
+public class PKN16 {
+
+	public static void main(String[] args) {
+		myFirstMethod();
+		myFirstMethod();
+		myFirstMethod();
+		myFirstMethod();
+		myFirstMethod();
+		myFirstMethod();
+	}
+	
+	public static void myFirstMethod(){
+		System.out.println("Hello Youtube");
+	}
+
+}
