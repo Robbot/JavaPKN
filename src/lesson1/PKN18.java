b41b802cae745751bc603d2cc178679dc1f65911
Rob_R <roju@robertrojek.pl>
2017-06-13 12:54:15 +0100

    minor

--- src/lesson1/PKN18.java
+++ src/lesson1/PKN18.java
@@ -23,12 +23,12 @@ public class PKN18 {
 		mark.setName("Mark");
 		mark.setAge(15);
 		
-		System.out.println(mark.name+" is "+mark.age+" years old.");
+		System.out.println(mark.getName()+" is "+mark.getAge()+" years old.");
 		tom.setId(2);
 		tom.setName("Tom");
 		tom.setAge(14);
 		
-		System.out.println(tom.name+" is "+tom.age+" years old.");
+		System.out.println(tom.getName()+" is "+tom.getAge()+" years old.");
 	
 	}
 
